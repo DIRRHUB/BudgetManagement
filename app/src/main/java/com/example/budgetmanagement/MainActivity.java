c96328f2120464253e25384f2b87484640fcd0f4
DIRR <bibishka117@gmail.com>
2022-01-05 15:08:13 +0200

    gradle fix

--- app/src/main/java/com/example/budgetmanagement/MainActivity.java
+++ app/src/main/java/com/example/budgetmanagement/MainActivity.java
@@ -3,7 +3,6 @@ package com.example.budgetmanagement;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-
 import com.example.budgetmanagement.databinding.ActivityMainBinding;
 
 
