a10439fc44551ba5f939d71add4fa3de582c499a
DIRR <bibishka117@gmail.com>
2022-02-16 17:34:33 +0200

    Updated sort types

--- app/src/main/java/com/example/budgetmanagement/SortPurchasesContent.java
+++ app/src/main/java/com/example/budgetmanagement/SortPurchasesContent.java
@@ -28,26 +28,34 @@ public class SortPurchasesContent {
     public SortPurchasesContent sort(){
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
             switch (sortType) {
-                case 0:
-                    arrayList.sort((t1, t2) -> {
-                        if (t1 == t2) {
-                            return 0;
-                        } else {
-                            return t1.name.compareTo(t2.name);
-                        }
-                    });
+                case 0://name a-z
+                    arrayList.sort((t1, t2) -> t1.name.compareTo(t2.name));
                     break;
-                case 1:
-                    arrayList.sort((t1, t2) -> {
-                        if (t1 == t2) {
-                            return 0;
-                        } else {
-                            return t2.name.compareTo(t1.name);
-                        }
-                    });
+                case 1://name z-a
+                    arrayList.sort((t1, t2) -> t2.name.compareTo(t1.name));
+                    break;
+                case 2://category a-z
+                    arrayList.sort((t1, t2) -> t1.category.compareTo(t2.category));
+                    break;
+                case 3://category z-a
+                    arrayList.sort((t1, t2) -> t2.category.compareTo(t1.category));
+                    break;
+                case 4://date 0-1
+                    arrayList.sort((t1, t2) -> t1.date.compareTo(t2.date));
+                    break;
+                case 5://date 1-0
+                    arrayList.sort((t1, t2) -> t2.date.compareTo(t1.date));
+                    break;
+                case 6://price 0-1
+                    break;
+                case 7://price 1-0
                     break;
             }
         }
         return this;
+    }
+
+    private void convertCurrencyToUSD() {
+
     }
 }
