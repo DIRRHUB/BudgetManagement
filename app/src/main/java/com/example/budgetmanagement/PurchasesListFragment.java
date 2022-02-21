e7501c2843a8ba85282ae8241be54781ecb161b2
DIRR <bibishka117@gmail.com>
2022-02-21 22:40:06 +0400

    Erasing purchase, added draw-style

--- app/src/main/java/com/example/budgetmanagement/PurchasesListFragment.java
+++ app/src/main/java/com/example/budgetmanagement/PurchasesListFragment.java
@@ -1,6 +1,7 @@
 package com.example.budgetmanagement;
 
 import android.content.Intent;
+import android.graphics.Canvas;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -10,6 +11,7 @@ import android.widget.AdapterView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -20,6 +22,8 @@ import com.google.android.material.snackbar.Snackbar;
 
 import java.util.ArrayList;
 import java.util.concurrent.TimeUnit;
+
+import it.xabaras.android.recyclerview.swipedecorator.RecyclerViewSwipeDecorator;
 
 public class PurchasesListFragment extends Fragment {
     private FragmentPurchasesListBinding binding;
@@ -111,6 +115,16 @@ public class PurchasesListFragment extends Fragment {
                     databaseContent.saveToDatabase(purchase, purchase.getPurchaseID());
                 }).show();
             }
+        }
+
+        @Override
+        public void onChildDraw(@NonNull Canvas c, @NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, float dX, float dY, int actionState, boolean isCurrentlyActive) {
+            new RecyclerViewSwipeDecorator.Builder(requireContext(), c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive)
+                    .addSwipeLeftBackgroundColor(ContextCompat.getColor(requireActivity(), R.color.red))
+                    .addSwipeLeftActionIcon(R.drawable.delete_icon)
+                    .create()
+                    .decorate();
+            super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
         }
     };
 
